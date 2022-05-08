6cb2d697223a65c0d363e167fbfe3946ef40b1ed
BilalSajid173 <86185413+BilalSajid173@users.noreply.github.com>
2022-05-08 12:45:28 +0530

    Update login.php

--- phpfiles/login.php
+++ phpfiles/login.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Log In</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="icon" href="images/favicon.ico.jpeg">
-    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="login.css">
     <script src="xx.js"></script>
 </head>
 
