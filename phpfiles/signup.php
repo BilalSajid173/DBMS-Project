0e058f31779c6c9e4579660ede0c466878cf579c
BilalSajid173 <86185413+BilalSajid173@users.noreply.github.com>
2022-05-08 14:58:00 +0530

    Update signup.php

--- phpfiles/signup.php
+++ phpfiles/signup.php
@@ -71,15 +71,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['studentid'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SignUP</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="icon" href="images/favicon.ico.jpeg">
-    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="signup.css">
     <script src="xx.js"></script>
 </head>
 
-<body style="background-color:rgb(230, 254, 255);">
-<center>
-<header class="headerofpage" style="position:relative; margin-right:20px;margin-top:15px;">
+<body">
+
+    <header class="headerofpage" style="position:relative; margin-right:20px;margin-top:15px;">
         <img src="../images/cover.png" alt="" class="logo">
         <nav>
             <ul class="navbar">
@@ -91,183 +92,84 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['studentid'])) {
         </nav>
         <a href="login.php" class="cta">Login</a>
     </header>
-</center>
-    <br>
-    <br><br><br><center>
+
+
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($showAlert == true) {
-            echo '<span style="color:white; background-color:green; font-size:25px;width:10px;"> Successfully submitted the details.<br><br></span>';
+            echo '<span style="color:white; background-color:green; font-size:25px;width:10px; text-align:center"> Successfully submitted the details.<br><br></span>';
         }
     }
     ?>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($showError == true && $exist == false) {
-            echo '<span style="color:white; background-color:red; font-size:25px;width:10px;"><b>Error!</b>  Passwords do not match.<br><br></span>';
+            echo '<span style="color:white; background-color:red; font-size:25px;width:10px; text-align:center"><b>Error!</b>  Passwords do not match.<br><br></span>';
         }
     }
     ?>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($showError == false && $exist == true) {
-            echo '<span style="color:white; background-color:red; font-size:25px;width:10px;"><b>Error!</b>  StudentID already taken.<br><br></span>';
+            echo '<span style="color:white; background-color:red; font-size:25px;width:10px; text-align:center"><b>Error!</b>  StudentID already taken.<br><br></span>';
         }
     }
     ?>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($showError == true && $exist == true) {
-            echo '<span style="color:white; background-color:red; font-size:25px;width:10px;"><b>Error!</b> Passwords do not match and StudentID already taken.<br><br></span>';
+            echo '<span style="color:white; background-color:red; font-size:25px;width:10px; text-align:center"><b>Error!</b> Passwords do not match and StudentID already taken.<br><br></span>';
         }
     }
     ?>
-    </center>
-    <center>
-    <div class="container">
+    <div class="signup-form-container">
 
-        <form class="well form-horizontal" action="signup.php" method="post" id="contact_form">
-            <fieldset>
-
-                <!-- Form Name -->
-                <legend>
-                    <center>
-                        <h2><b>Registration Form</b></h2>
-                    </center>
-                </legend><br><br>
-                <!-- Text input-->
-
-                <div class="form-group">Student Name</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input required="" name="studentname" placeholder="First Name" class="form-control" type="text">
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Student ID</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                            <input required="" name="studentid" placeholder="Student ID" class="form-control" type="number">
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">Roll Number</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input required="" name="rollno" placeholder="Roll Number" class="form-control" type="text">
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-4 control-label">E-Mail</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                            <input  required="" name="email" placeholder="E-Mail Address" class="form-control" type="email">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Department / Office</label>
-                    <div class="col-md-4 selectContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
-                            <select  required="" name="deptname" class="form-control selectpicker">
-                                <option value="">Select your Department/Office</option>
-                                <option>Computer Science</option>
-                                <option>Electronics & Communication</option>
-                                <option>Electrical Engineering</option>
-                                <option>Mechanical Engineering</option>
-                                <option>Civil Engineering</option>
-                                <option>BA.Economics(Hons)</option>
-                                <option>B.Com</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Gender</label>
-                    <div class="col-md-4 selectContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
-                            <select  required="" name="gender" class="form-control selectpicker">
-                                <option value="">Gender</option>
-                                <option>Male</option>
-                                <option>Female</option>
-                                <option>Other</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Text input-->
+        <div class="wrapper-signup">
 
 
-                <!-- Text input-->
+            <h2>Registration Form</h2>
 
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Password</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input  required="" name="password" placeholder="Password" class="form-control" type="password">
-                        </div>
-                    </div>
-                </div>
+            <form action="signup.php" method="post" id="contact_form">
 
-                <!-- Text input-->
+                <input required="" name="studentname" placeholder="First Name" type="text">
 
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Confirm Password</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input required=""  name="cpassword" placeholder="Confirm Password" class="form-control" type="password">
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Contact No.</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                            <input required=""  name="contactno" placeholder="Contact No." class="form-control" type="number">
-                        </div>
-                    </div>
-                </div>
+                <input required="" name="studentid" placeholder="Student ID" type="number">
 
-                <!-- Text input-->
-                
+                <input required="" name="rollno" placeholder="Roll Number" type="text">
 
+                <input required="" name="email" placeholder="E-Mail Address" type="email">
 
-                <!-- Text input-->
+                <select required="" name="deptname">
+                    <option value="">Select your Department/Office</option>
+                    <option>Computer Science</option>
+                    <option>Electronics & Communication</option>
+                    <option>Electrical Engineering</option>
+                    <option>Mechanical Engineering</option>
+                    <option>Civil Engineering</option>
+                    <option>BA.Economics(Hons)</option>
+                    <option>B.Com</option>
+                </select>
 
-               
+                <select required="" name="gender">
+                    <option value="">Gender</option>
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>Other</option>
+                </select>
 
-                <!-- Select Basic -->
+                <input required="" name="password" placeholder="Password" type="password">
 
-               
-                <!-- Button -->
-                <div class="form-group">
-                    <label class="col-md-3 control-label"></label>
-                    <div class="col-md-3"><br>
-                        <button type="submit" class="btn btn-warning" style="position:relative; margin-right:-5px;">SUBMIT <span class="glyphicon glyphicon-send"></span></button>
-                    </div>
-                </div>
+                <input required="" name="cpassword" placeholder="Confirm Password" type="password">
 
-            </fieldset>
-        </form>
-        </center>
+                <input required="" name="contactno" placeholder="Contact No." type="number">
+
+                <button type="submit" class="btn btn-warning">SUBMIT</button>
+
+            </form>
+
+        </div>
+
     </div>
-    </div><!-- /.container -->
-</body>
+    </body>
 
 </html>
